17b867267ed4cc2f4bce2a66629b5730ae12d509
rb14014 <46453093+rb14014@users.noreply.github.com>
2024-06-12 07:23:01 +0200

    Usunięcie komentarzy w functions

--- functions.py
+++ functions.py
@@ -4,8 +4,6 @@ def get_todos(filepath=FILE_PATH):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
-
-# print(help(get_todos))
 def write_todos(todos_arg, filepath=FILE_PATH):
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
